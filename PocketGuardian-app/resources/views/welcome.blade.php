6b875200c7cbed6d3438e51333822de902830661
SoyMarioHdez-U <mario.hernandez2@catolica.edu.sv>
2023-11-13 14:51:14 -0600

    PROBANDO

--- PocketGuardian-app/resources/views/welcome.blade.php
+++ PocketGuardian-app/resources/views/welcome.blade.php
@@ -39,7 +39,7 @@
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
-                    <p class="titulo"><b>POCKET GUARDIAN</b></p>
+                    <p class="titulo"><b>POCKET GUARDIAN - UNICAES</b></p>
                 </div>
 
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
