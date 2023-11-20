faa3b031cb1de499a3210c4d8902f76b4d8617a4
alerodriguez23 <jenniffer.rodriguez@catolica.edu.sv>
2023-11-20 11:47:09 -0600

    ventana cuenta

--- HTML-CSS-JS/html/Cuentas.php
+++ HTML-CSS-JS/html/Cuentas.php
@@ -128,20 +128,22 @@
             
             </div>
 
-
-           <section class="modal_cuenta">
+            
+           <div class="modal_cuenta">
+          
             <div class="modal_containerC">
               <h2 class="titulo-modal">Crear Nueva Cuenta</h2>
-              <i class='bx bxs-wallet'>Cuenta</i>
+              <i class='bx bxs-wallet'></i>
               <input class="datos"  type="texto"  placeholder="Nombre de Cuenta" nombre="Nueva_cuenta">
               <input type="button" class="boton" id="Agregar" value="Añadir">
               <input type="button" class="boton" id="cancelar" value="Cancelar">
+              
             </div>  
         
 
 
 
-           </section>
+    </div>
 
 
            </div>
