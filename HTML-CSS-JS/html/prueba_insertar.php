fb5be9665c41770f10999e34312dc853871006fb
SoyMarioHdez-U <mario.hernandez2@catolica.edu.sv>
2023-11-20 10:26:28 -0600

    Ya se puede añadir cuentas (desde form test)

---
+++ HTML-CSS-JS/html/prueba_insertar.php
@@ -0,0 +1,88 @@
+<?php
+
+session_start();
+  include '../php/conexion_be.php';
+  include '../php/procesos_cuenta_be.php';
+  include '../php/Objetos.php';
+
+
+  if(!isset($_SESSION['usuario'])){
+    echo '
+      <script>
+        alert("Por favor, inicia sesión");
+      </script>
+    ';
+    header("location: ../html/login.php");
+    session_destroy();
+    die();
+
+    
+  }
+  $correo_electronico = $_SESSION['usuario'];
+      //Obtenemos nombre y apellido del usuario para mostrarlo
+      $nombre_usuario = obtenerNombre($conexion, $correo_electronico);
+      $apellido_usuario = obtenerApellido($conexion, $correo_electronico);
+      
+      //Obtenemos la cantidad de cuentas que tiene el usuario
+      $total_cuentas = obtenerNumeroCuentas($conexion, $correo_electronico);
+  //    echo "El usuario $nombre_usuario $apellido_usuario tiene $total_cuentas cuenta(s).";
+
+      //Obtener el ID para luego mostrar el total de su cuenta
+      $id_usuario = obtenerID($conexion, $correo_electronico);
+      //echo " | Id del usuario es: $id_usuario";
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Insertar cuentas y registros</title>
+</head>
+<body>
+    
+<!--Formulario de login y registro-->
+<div class="form-container2">
+               
+               <form action="http://localhost/PocketGuardian-UNICAES/HTML-CSS-JS/php/insertar_cuentas_be.php" method="POST" class="formulario-login">
+                   <h1>PocketGuardian</h1>
+                   <h2>Añadir cuenta </h2>
+                   Nombre de la cuenta: <input class="datos" type="text" placeholder="Nombre"  name="nombre_cuenta">
+                   
+                   
+                   <button class="botones">Añadir</button>
+   
+
+   
+                   
+                
+   
+               </form>
+
+               <form action="http://localhost/PocketGuardian-UNICAES/HTML-CSS-JS/php/registro_usuario_be.php" method="POST" class="formulario-login">
+                   <h1>PocketGuardian</h1>
+                   <h2>Añadir transacción</h2>
+                   Monto: $ <input class="datos" type="number" inputmode="numeric" placeholder="Monto"  name="monto">
+                   Descripción: <input class="datos" type="text" placeholder="Descripción"  name="descripción">
+                   <select name="select">
+                        <option value="value1">Cuenta 1</option>
+                        <option value="value2" selected>Cuenta 2</option>
+                        <option value="value3">Cuenta 3</option>
+                    </select>
+                   
+                   <button class="botones">Añadir</button>
+   
+   
+                   
+                
+   
+               </form>
+           </div>
+
+    
+</body>
+</html>
---
+++ HTML-CSS-JS/php/insertar_cuentas_be.php
@@ -0,0 +1,68 @@
+<?php
+    session_start();
+
+    include 'conexion_be.php';
+    include 'procesos_cuenta_be.php';
+    
+    $correo_electronico = $_SESSION['usuario'];
+
+      //Obtener el ID para luego mostrar el total de su cuenta
+      $id_usuario = obtenerID($conexion, $correo_electronico);
+      //echo " | Id del usuario es: $id_usuario";
+
+
+
+    $nombre_cuenta = $_POST['nombre_cuenta'];
+    
+
+    //Validar que todos los campos estén llenos
+    if (empty($nombre_cuenta)) {
+        echo '
+            <script>
+                alert("Por favor llena todos los campos.");
+                window.location = "../html/prueba_insertar.php";
+            </script>
+        ';
+        exit(); 
+    }
+
+    $query = "INSERT INTO cuenta(nombre_cuenta, id_usuario) 
+              VALUES('$nombre_cuenta', '$id_usuario')";
+
+    //verificar que no se repita el nombre de la cuenta
+    $verificar_cuenta = mysqli_query($conexion, "SELECT * FROM cuenta WHERE id_usuario='$id_usuario' AND nombre_cuenta='$nombre_cuenta';");
+
+    if(mysqli_num_rows($verificar_cuenta)>0){
+        echo '
+        <script>
+            alert("Usted ya posee una cuenta con ese nombre, ingrese uno nuevo.");
+            window.location = "../html/prueba_insertar.php";
+        </script>
+        ';
+        mysqli_close($conexion);
+        
+    }
+
+
+
+    $ejecutar = mysqli_query($conexion, $query);
+
+    if($ejecutar){
+        echo '
+        <script>
+            alert("Cuenta almacenada con éxito");
+            window.location = "../html/Cuentas.php";
+        </script>
+        ';
+    }
+    else{
+        echo '
+        <script>
+            alert("Algo salió mal :( Intenta de nuevo.")
+        </script>
+    ';
+    }
+
+    mysqli_close($conexion);
+
+?>
