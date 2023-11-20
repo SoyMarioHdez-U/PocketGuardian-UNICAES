223c8ef843a7773aa410e04909bb0326791f98f3
SoyMarioHdez-U <mario.hernandez2@catolica.edu.sv>
2023-11-19 22:45:48 -0600

    Ya funcionan las cuentas con sus transacciones. :)

--- HTML-CSS-JS/html/Cuentas.php
+++ HTML-CSS-JS/html/Cuentas.php
@@ -1,6 +1,6 @@
 
 <?php
-/*
+
   session_start();
   include '../php/conexion_be.php';
   include '../php/procesos_cuenta_be.php';
@@ -38,7 +38,7 @@
 //        echo $nombre . "<br>";
     }
 
-  */
+  
 ?>
 
 
@@ -52,7 +52,7 @@
         
         <link rel="icon" href="../Png/pgLogo1" type="image/png">
 
-
+        
         <link rel="stylesheet" href="http://localhost/PocketGuardian-UNICAES/HTML-CSS-JS/css/estilos.css">
         <link rel="stylesheet" href="http://localhost/PocketGuardian-UNICAES/HTML-CSS-JS/css/cheques.css">
         <script type='text/javascript' src='http://localhost/PocketGuardian-UNICAES/HTML-CSS-JS/js/sidebar.js'></script>
--- HTML-CSS-JS/php/Objetos.php
+++ HTML-CSS-JS/php/Objetos.php
@@ -4,11 +4,13 @@
         public $monto;
         public $descripcion;
         public $fecha;
+        public $tipo_movimiento;
 
-        public function __construct($monto, $descripcion, $fecha) {
+        public function __construct($monto, $descripcion, $fecha, $tipo_movimiento) {
             $this->monto = $monto;
             $this->descripcion = $descripcion;
             $this->fecha = $fecha;
+            $this->tipo_movimiento = $tipo_movimiento;
         }
     }
 
@@ -17,15 +19,15 @@
         public $id_cuenta;
         public $nombre_cuenta;
         public $saldo;
-        public $transacciones; // Array para almacenar objetos de tipo Transaccion
+        public $transacciones =array(); // Array para almacenar objetos de tipo Transaccion
         
 
 
-        public function __construct($id_cuenta, $nombre_cuenta, $saldo = 0, $transacciones) {
+        public function __construct($id_cuenta, $nombre_cuenta, $saldo, $transacciones) {
             $this->id_cuenta = $id_cuenta;
             $this->nombre_cuenta = $nombre_cuenta;
             $this->saldo = $saldo;
-            $this->transacciones = array(); // Inicializar el array de transacciones
+            $this->transacciones = $transacciones; // Inicializar el array de transacciones
         }
     }
 
